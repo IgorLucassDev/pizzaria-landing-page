0781883a0f86cdcd129a157fd58f25df05f1b0ca
IgorLucasDev <62673756+IgorLucassDev@users.noreply.github.com>
2024-01-17 17:18:02 -0300

    new header
    header fix and logic improvements

--- js/header.js
+++ js/header.js
@@ -1,17 +1,34 @@
-let menuButton = document.querySelector("#menu-btn")
-let closeButton = document.querySelector("#close-btn")
-menuButton.addEventListener('click', menuToggle)
+var openButton = document.querySelector('.menu-btn')
+openButton.addEventListener('click', openMenu)
+
+var closeButton = document.querySelector('.close-menu')
 closeButton.addEventListener('click', closeMenu)
-let myNav = document.querySelector('.nav-itens')
+
+var Menu = document.querySelector('.mobile-menu')
+
+var Content = document.querySelector('#main-content')
+
+var Landscape = document.querySelector('.landscape-img')
+
+var Alert = document.querySelector('.alert')
+
+var Card = document.querySelector('.cards')
 
 
-
-function menuToggle(){  
-    myNav.style.display = 'block'
-    myNav.style.right = '0'
+function openMenu(){
+    Menu.style.display = 'flex'
+    Landscape.style.webkitFilter = 'blur(5px)';
+    Alert.style.webkitFilter = 'blur(5px)';
+    Card.style.webkitFilter = 'blur(5px)';
+    document.body.style.overflow = 'hidden'
+    Content.addEventListener('click', closeMenu)
 }
 
 function closeMenu(){
-    myNav.style.right = '-300px'
-    myNav.style.display = 'none'
+    Menu.style.display = 'none'
+    Landscape.style.webkitFilter = 'blur(0px)';
+    Alert.style.webkitFilter = 'blur(0px)';
+    Card.style.webkitFilter = 'blur(0px)';
+    document.body.style.overflow = 'auto'
 }
+
